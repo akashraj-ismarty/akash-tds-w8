472e8b0c0f8cba0da9c6b63f7e891d04a2ba82f3
akash-ismarty <kakashraj011@gmail.com>
2022-11-30 23:04:06 +0530

    YES

--- app.py
+++ app.py
@@ -6,20 +6,23 @@ def welcome():
     return "Welcome All"
 
 def main():
-  st.title("Addition")
+  st.title("EVEN or ODD")
   html_temp = """
   <div style="background-color:tomato;padding:10px">
-  <h2 style="color:white;text-align:center;">Addition of 2 numbers using Streamlit</h2>
+  <h2 style="color:blue;text-align:center;">Finding even or odd number using Streamlit</h2>
   </div>
   """
   st.markdown(html_temp,unsafe_allow_html=True)
-  num1 = st.number_input("Number 1")
-  num2 = st.number_input("Number 2")
-  result = num1 + num2
-  st.success('The output is {}'.format(result))
+  
+  num1 = st.number_input("Please enter the number here:")
+  if (num1%2==0):
+    st.success('{} is an even number'.format(num1))
+  else:
+    st.success('{} is an odd number'.format(num1))
+  
   if st.button("Made By"):
-      st.text("Atharva Ramgirkar")
-      st.text("22ds1000188")
+      st.text("K AKASH")
+      st.text("21F1004520")
 
 if __name__=='__main__':
   main()
